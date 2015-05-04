8b2882a007af601a4801a254109963f319845b98
JonishNing <patopato@yeah.net>
2015-05-04 10:11:35 +0800

    aaaaa

--- DensityMining/src/Commons/GetPeriod.java
+++ DensityMining/src/Commons/GetPeriod.java
@@ -1,5 +1,7 @@
 package Commons;
 
+import java.sql.Timestamp;
+
 import Model.Grid_Summary;
 import Commons.TimeSwitch;
 import Commons.DataAnalysis;
@@ -15,10 +17,10 @@ public class GetPeriod
 	public static void main(String [] args)
 	{
 		GetPeriod Period=new GetPeriod();
-		Grid_Summary Summary=new Grid_Summary(1500000,200000,10000,468202301);
+		Grid_Summary Summary=new Grid_Summary(1500000,200000,10000,468562301);
 		TimeSwitch Switch=new TimeSwitch();
 		System.out.println(Switch.getLocalTime(Summary.getGPSTime(), Summary.getLongitude()));
-		
+		System.out.println(new Timestamp(468533501));
 		DataAnalysis Analysis=new DataAnalysis();
 		//System.out.println(Analysis.judge_dayornight(Summary.getLongitude(), Summary.getLatitude(), Switch.getLocalTime(Summary.getGPSTime(), Summary.getLongitude())));
 		//System.out.println(Summary.getGPSTime());
